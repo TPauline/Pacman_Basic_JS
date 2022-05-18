da28fd5170fc865f742b11c134c2ae2ec60ba8f8
TPauline <73195686+TPauline@users.noreply.github.com>
2022-05-18 04:48:30 -0400

    Add files via upload

--- pacman.js
+++ pacman.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const width = 21
-    const height = 21
     const grid = document.querySelector('.grid')
-    const gList = []
-    pacman = { x: 9, y: 0, direction: 'ArrowRight', points: 0, lives: 3 }
-    ghostsLst = []
-    directions = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
+    const bottom = document.querySelector('.bottom')
+    document.body.style.zoom = "85%"
 
+    // grid.parentNode.style.marginLeft = ((window.innerWidth - 720) / 2).toString() + "px"
+
+    const gList = []
+    pacman = { x: 9, y: 0, direction: 'ArrowRight', points: 0, lives: 4, eaten: 0, gameStarted: false }
+    intervals = {}
+    directions = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
 
     class Ghosts {
         constructor(character, nickname, x, y, direction, scared, speed) {
@@ -17,14 +20,17 @@ document.addEventListener('DOMContentLoaded', () => {
             this.direction = direction
             this.scared = scared
             this.speed = speed
+            this.initialX = x
+            this.initialY = y
         }
     }
 
-    ghostsLst.push(new Ghosts("SHADOW", "BLINKY", 7, 10, "ArrowLeft", false, 500))
-    ghostsLst.push(new Ghosts("SEEDY", "PINKY", 9, 10, "ArrowUp", false, 500))
-    ghostsLst.push(new Ghosts("BASHFUL", "INKY", 9, 9, "ArrowUp", false, 500))
-    ghostsLst.push(new Ghosts("POKEY", "CLYDE", 9, 11, "ArrowUp", false, 500))
-
+    ghostsLst = [
+        new Ghosts("SHADOW", "BLINKY", 7, 9, "ArrowLeft", false, 500),
+        new Ghosts("SEEDY", "PINKY", 9, 9, "ArrowUp", false, 500),
+        new Ghosts("BASHFUL", "INKY", 9, 8, "ArrowUp", false, 500),
+        new Ghosts("POKEY", "CLYDE", 9, 10, "ArrowUp", false, 500)
+    ]
 
     const legend = {
         0: "empty",
@@ -36,32 +42,34 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const gridMap = [
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0],
-        [0, 1, 3, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 3, 1, 0],
-        [0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0],
-        [0, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 0],
-        [0, 1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1, 0],
-        [0, 1, 1, 1, 1, 2, 1, 1, 1, 0, 1, 0, 1, 1, 1, 2, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 2, 1, 0, 1, 1, 5, 1, 1, 0, 1, 2, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 2, 0, 0, 1, 4, 4, 4, 1, 0, 0, 2, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 0, 1, 2, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 0, 1, 2, 1, 1, 1, 1, 0],
-        [0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0],
-        [0, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 0],
-        [0, 1, 3, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 3, 1, 0],
-        [0, 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1, 0],
-        [0, 1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1, 0],
-        [0, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 0],
-        [0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, ],
+        [1, 3, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 3, 1, ],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, ],
+        [1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, ],
+        [1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1, ],
+        [1, 1, 1, 1, 2, 1, 1, 1, 0, 1, 0, 1, 1, 1, 2, 1, 1, 1, 1, ],
+        [0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, ],
+        [1, 1, 1, 1, 2, 1, 0, 1, 1, 5, 1, 1, 0, 1, 2, 1, 1, 1, 1, ],
+        [0, 0, 0, 0, 2, 0, 0, 1, 4, 4, 4, 1, 0, 0, 2, 0, 0, 0, 0, ],
+        [1, 1, 1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 0, 1, 2, 1, 1, 1, 1, ],
+        [0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, ],
+        [1, 1, 1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 0, 1, 2, 1, 1, 1, 1, ],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, ],
+        [1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, ],
+        [1, 3, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 3, 1, ],
+        [1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1, ],
+        [1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1, ],
+        [1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, ],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, ],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
     ]
     console.log(gridMap)
 
     function buildSetting() {
         gridMap.forEach(row => {
+            // row.pop()
+            // row.unshift()
             let t = []
             row.forEach(col => {
                 // console.log(col)
@@ -71,6 +79,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 div.classList.add(legend[col])
                 grid.appendChild(div)
                 t.push(div)
+                if (col == 2 || col == 3) { pacman.eaten++ }
+
             });
             gList.push(t)
         });
@@ -78,12 +88,18 @@ document.addEventListener('DOMContentLoaded', () => {
         ghostsLst.forEach(g => {
             gList[g.x][g.y].classList.add(g.nickname)
             gList[g.x][g.y].classList.add("ghost")
-
+            const div = document.createElement("div");
+            const innerDiv = document.createElement("div");
+            div.appendChild(innerDiv);
+            div.classList.add("pacman")
+            bottom.appendChild(div)
         });
+        console.log(bottom)
+
+        console.log("pacman.eaten " + pacman.eaten)
+        gList[pacman.x][pacman.y].classList.add("pacman")
     }
     buildSetting();
-
-    gList[pacman.x][pacman.y].classList.add("pacman")
 
     function movePacman(e) {
 
@@ -93,14 +109,14 @@ document.addEventListener('DOMContentLoaded', () => {
             var xDiff = xTouch - xUp;
             var yDiff = yTouch - yUp;
             if (Math.abs(xDiff) > Math.abs(yDiff)) { /*most significant*/
-                if (xDiff > 0 && pacman.direction != "ArrowRight") /* left swipe */
+                if (xDiff > 0) /* left swipe */
                     pacman.direction = 'ArrowLeft';
-                else if (xDiff <= 0 && pacman.direction != "ArrowLeft") /* right swipe */
+                else if (xDiff <= 0) /* right swipe */
                     pacman.direction = 'ArrowRight';
             } else {
-                if (yDiff > 0 && pacman.direction != "ArrowDown") /* up swipe */
+                if (yDiff > 0) /* up swipe */
                     pacman.direction = 'ArrowUp';
-                else if (xDiff <= 0 && pacman.direction != "ArrowUp") /* down swipe */
+                else if (xDiff <= 0) /* down swipe */
                     pacman.direction = 'ArrowDown';
             } /* reset values */
             xTouch = null;
@@ -146,16 +162,71 @@ document.addEventListener('DOMContentLoaded', () => {
         if (gList[pacman.x][pacman.y].classList.contains("dots")) {
             gList[pacman.x][pacman.y].classList.remove("dots")
             pacman.points += 10;
+            pacman.eaten--;
         }
 
         if (gList[pacman.x][pacman.y].classList.contains("energizers")) {
             gList[pacman.x][pacman.y].classList.remove("energizers")
             pacman.points += 50;
+            pacman.eaten--;
+            ghostsLst.forEach(g => {
+                g.scared = true
+                gList[g.x][g.y].classList.add("scared")
+            });
+        }
+        if (!pacman.eaten) { alert("LEVEL UP!") }
+
+    }
+
+    function scared(ghost) {
+        if (ghost.x == pacman.x && ghost.y == pacman.y) {
+            if (ghost.scared) {
+                gList[pacman.x][pacman.y].classList.remove("ghost")
+                gList[pacman.x][pacman.y].classList.remove("scared")
+                gList[pacman.x][pacman.y].classList.remove(ghost.nickname)
+                pacman.points += 200
+                ghost.x = ghost.initialX
+                ghost.y = ghost.initialY
+            } else {
+                console.log("!!!!!!!!!!")
+                gList[pacman.x][pacman.y].classList.remove("pacman")
+                pacman.lives--;
+                bottom.removeChild(bottom.lastChild);
+                pacman.x = 9
+                pacman.y = 0
+                pacman.gameStarted = false
+
+                ghostsLst.forEach(ghost => {
+                    gList[ghost.x][ghost.y].classList.remove("ghost")
+                    gList[ghost.x][ghost.y].classList.remove("scared")
+                    gList[ghost.x][ghost.y].classList.remove(ghost.nickname)
+                    ghost.x = ghost.initialX
+                    ghost.y = ghost.initialY
+
+                });
+                gList[pacman.x][pacman.y].classList.add("pacman")
+
+                if (pacman.lives == 0) {
+                    clearInterval(intervals["movePacman"], 500)
+                    gList[pacman.x][pacman.y].classList.remove("pacman")
+                    alert("Game Over!")
+                }
+
+            }
         }
     }
 
+    function ghostMode() {
+        ghostsLst.forEach(e => { if (e.scared) e.scared = false })
+    }
+
     function moveGhosts(ghost) {
-        ghost.direction = directions[Math.floor(Math.random() * directions.length)]
+        if (pacman.gameStarted) {
+            ghost.direction = directions[Math.floor(Math.random() * directions.length)]
+        }
+        gList[ghost.x][ghost.y].classList.remove("scared")
+
+        pacman.gameStarted = true
         switch (ghost.direction) {
             case "ArrowUp":
                 if (ghost.x > 0 && !gList[ghost.x - 1][ghost.y].classList.contains("wall") && !gList[ghost.x - 1][ghost.y].classList.contains("ghost")) {
@@ -199,8 +270,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
                 break;
         }
+        if (ghost.scared) {
+            console.log("^^^")
+            gList[ghost.x][ghost.y].classList.add("scared")
+        }
     }
-
 
     function touchStarted(e) {
         const initialTouch = (e.touches || e.originalEvent.touches)[0];
@@ -212,11 +286,14 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('touchmove', movePacman, false);
 
     window.addEventListener('load', () => {
-        setInterval(movePacman, 500)
-        setInterval(eat, 50)
-        ghostsLst.forEach(g => setInterval(() => { moveGhosts(g) }, 500));
+        intervals["movePacman"] = setInterval(movePacman, 500)
+        intervals["eat"] = setInterval(eat, 500)
+        intervals["ghostMode"] = setInterval(ghostMode, 25600)
+
+        ghostsLst.forEach(g => {
+            intervals["moveGhosts"] = setInterval(() => { moveGhosts(g) }, 500)
+            intervals["scared"] = setInterval(() => { scared(g) }, 500)
+        });
     })
-
     document.addEventListener('keyup', (e) => { pacman.direction = e.code })
-
 })
